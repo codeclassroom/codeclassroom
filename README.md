f33dbb95099cbaf8e95bdd896ea4e06c17ab4622
Bhupesh-V <varshneybhupesh@gmail.com>
2019-10-23 19:31:36 +0530

    basic struture

---
+++ README.md
@@ -0,0 +1,51 @@
+# codeclassroom
+
+> codeclassroom is back-end of the "CodeClassroom" project.
+
+
+### Prerequisties
+- Python 3.6.8+
+- virtualenv
+
+### Installation 🔮
+
+1. Create Virtual Environment.
+```bash
+virtualenv -p python3 cc && cd cc && source bin/activate
+```
+2. Clone repository.
+```bash
+git clone https://github.com/codeclassroom/codeclassroom.git
+```
+3. Install Dependencies.
+```bash
+pip3 install -r requirements.txt
+```
+4. Migrate Changes.
+```bash
+python3 manage.py migrate
+```
+5. Run tests.
+```bash
+python3 manage.py test
+```
+6. Run Server
+```bash
+python3 manage.py runserver
+```
+
+
+## Author(s)
+
+👥 **Bhupesh Varshney**
+
+- Twitter: [@bhupeshimself](https://twitter.com/bhupeshimself)
+- DEV: [bhupesh](https://dev.to/bhupesh)
+
+👥 **Gagan Singh**
+
+- GitHub: [GAGANsinghmsitece](https://github.com/GAGANsinghmsitece)
+
+👥 **Animesh Ghosh**
+
+- GitHub: [Animesh-Ghosh](https://github.com/Animesh-Ghosh)
---
+++ app/models.py
@@ -0,0 +1,92 @@
+'''Jointly developed by Gagan Singh, Aniemsh Ghosh 
+	& Bhupesh Varshney
+'''
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Student(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, blank=True)
+    profile_pic = models.ImageField(upload_to='StudentProfilePic', blank=True)
+    college_name = models.CharField(max_length=200, blank=True)
+    course = models.CharField(max_length=50, blank=True)
+    roll_no = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Professor(models.Model):
+    '''Assuming a professor can create multiple classrooms and each classroom can have 
+    multiple assignments. if a classroom is deleted, all of the assignments of that class will
+    delete automatically*.
+    '''
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, blank=True)
+    profile_pic = models.ImageField(upload_to='ProfessorProfilePic', blank=True)
+    college_name = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Classroom(models.Model):
+    professor = models.ForeignKey(to=Professor, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=False)
+    students = models.ManyToManyField(to=Student, blank=True)
+    '''because a student can join multiple classroom and each classroom can have multiple 
+    students'''
+
+    class Meta:
+        verbose_name = 'Classroom'
+        verbose_name_plural = 'Classrooms'
+
+    def __str__(self):
+        return self.title
+
+
+class Assignment(models.Model):
+    classroom = models.ForeignKey(to=Classroom, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=False)
+    deadline = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    assignment = models.ForeignKey(to=Assignment, on_delete=models.CASCADE)
+    title = models.CharField(max_length=500, blank=True)
+    description = models.TextField(blank=True)
+    sample_input = models.TextField(blank=True)
+    sample_output = models.TextField(blank=True)
+    marks = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Solution(models.Model):
+    ACCEPTED = 'accepted'
+    PARTIALLY_SUBMIT = 'partially-submit'
+    WRONG = 'wrong'
+    NOT_ATTEMPT = 'not-attempted'
+
+	STATUS = (
+        (ACCEPTED, 'Accepted'),
+        (PARTIALLY_SUBMIT, 'Partially Submitted'),
+        (WRONG, 'Wrong Answer'),
+        (NOT_ATTEMPT, 'Not Attempted')
+    )
+
+    question = models.ForeignKey(to=Question, on_delete=models.CASCADE)
+    student = models.ForeignKey(to=Student, on_delete=models.CASCADE)
+    sub_date = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=100, choices=STATUS)
+    submission = models.FileField(
+        upload_to=f'question{question}/submissions/{student}',
+        blank=False
+    )
+    remark = models.CharField(max_length=500, blank=True)
+    # this field may be filled by prof as remark
