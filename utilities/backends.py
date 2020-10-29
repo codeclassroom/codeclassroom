160113970d32398479264eeea1fa37ddf6a6d9a2
Keerthi Naredla <keerthi4308@gmail.com>
2020-10-29 07:59:21 -0700

    Custom authentication backends to support email/username for login

---
+++ utilities/backends.py
@@ -0,0 +1,32 @@
+"""Custom authentication backends to support email/username for login"""
+
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+
+class UsernameOrEmailBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None:
+            username = kwargs.get(UserModel.USERNAME_FIELD)
+
+        if '@' in username:
+            kwargs = {'email': username}
+        else:
+            kwargs = {'username': username}
+        try:
+            user = UserModel.objects.get(**kwargs)
+        except UserModel.DoesNotExist:
+            return None
+
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
+
+    def get_user(self, user_id):
+        try:
+            user = UserModel.objects.get(pk=user_id)
+        except UserModel.DoesNotExist:
+            return None
+
+        return user if self.user_can_authenticate(user) else None
